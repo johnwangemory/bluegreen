2feb038a0d2cc72d8a9236c8a8f624f44ea252e6
johnwangemory <john.wang@emory.edu>
2019-10-17 19:53:53 -0400

    Update image.php

--- image.php
+++ image.php
@@ -1,6 +1,6 @@
 <?php
 // Create a 55x30 image
-$im = imagecreatetruecolor(200, 200);
+$im = imagecreatetruecolor(200, 201);
 $white = imagecolorallocate($im, 255, 255, 255);
 $blue = imagecolorallocate($im, 0, 0, 255);
 $green = imagecolorallocate($im, 0, 255, 0);
